370e8e2c67bae6b7c09673bdfdd51aede6d96bbc
Luiz Henrique A. Mello <lhamello@gmail.com>
2020-04-11 00:55:40 -0300

    migrado de JavaEE para Spring

---
+++ src/main/java/br/com/lhamello/gerenciadorfinancaspessoais/GerenciadorFinancasPessoaisApplication.java
@@ -0,0 +1,13 @@
+package br.com.lhamello.gerenciadorfinancaspessoais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorFinancasPessoaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorFinancasPessoaisApplication.class, args);
+	}
+
+}
