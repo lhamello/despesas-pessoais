475e6bae758c18af432165d74e055fa9ac1ef621
Luiz Henrique A. Mello <luiz-mello@procergs.rs.gov.br>
2022-01-30 06:23:07 -0300

    criado projeto

---
+++ src/test/java/br/com/lhamello/gerenciadorfinancaspessoais/GerenciadorFinancasPessoaisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lhamello.gerenciadorfinancaspessoais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorFinancasPessoaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
